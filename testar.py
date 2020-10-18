0d0e9bfe8c4458a5990e2276e2ac1c08b808baa5
Emmanuel <30930846+EmmanuelCasangel@users.noreply.github.com>
2020-10-17 21:06:08 -0300

    primeira versão
    a melhorar:
    deixar dinamico quantos arquivos ele vai usar para a sequencia de testes

---
+++ testar.py
@@ -0,0 +1,26 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description="Testa os labs")
+parser.add_argument('-l', '--lab-number', type=str, help="O numero do lab que voce quer testar (01, 02, ...)", required=True)
+args = parser.parse_args()
+
+lab_number = args.lab_number
+dir = "./testes/lab" + lab_number + "/"
+
+if not os.path.exists(dir):
+  os.makedirs(dir)
+
+for i in range (1, 10):
+
+    executar_s = "python3  lab"+ lab_number +".py < aux"+ lab_number + "/open/arq0"+ str(i) +".in > "+ dir + "rq0" + str(i) + ".result"
+    comparar_s = "diff aux" + lab_number + "/open/arq0" + str(i) + ".out "+ dir + "rq0" + str(i) + ".result"
+
+    print("Linhas diferentes nas saidas:")
+    print("\n")
+
+    os.system(executar_s)
+    os.system(comparar_s)
+
+    print("\n\n\n\n")
+
